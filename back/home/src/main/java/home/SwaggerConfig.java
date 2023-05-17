edeb05bd549ffd2c388cc547bc9d413e4c631e7b
leejunejae <oune@kumoh.ac.kr>
2023-05-17 17:20:41 +0900

    feat: 스웨어 설정 추가

---
+++ back/home/src/main/java/home/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package home;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+@EnableWebMvc
+public class SwaggerConfig {
+    @Bean
+    public Docket swaggerApi() {
+        return new Docket(DocumentationType.OAS_30)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("home.qna.controller"))
+                //.apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(mySwaggerInfo());
+    }
+
+    private ApiInfo mySwaggerInfo() {
+        return new ApiInfoBuilder()
+                .title("SwaggerTest API")
+                .description("SwaggerTest API Docs")
+                .build();
+    }
+    
+    @Bean
+    public InternalResourceViewResolver defaultViewResolver() {
+        return new InternalResourceViewResolver();
+    }
+
+}

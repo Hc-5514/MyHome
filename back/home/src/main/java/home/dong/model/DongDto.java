823d27b212d16a111d6d267198e2cf5b6f145392
Hc-5514 <tbd05050@naver.com>
2023-05-18 01:02:08 +0900

    feat: DongDto 추가

---
+++ back/home/src/main/java/home/dong/model/DongDto.java
@@ -0,0 +1,16 @@
+package home.dong.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DongDto {
+    private String sidoName;
+    private String gugunName;
+    private String dongName;
+}

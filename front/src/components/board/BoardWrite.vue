fabc11597c210048d8a1432ff8e90671154ac08b
Hc-5514 <tbd05050@naver.com>
2023-05-24 15:49:11 +0900

    feat: BoardWrite.vue, BoardUpdate.vue, BoardInputItem.vue 추가

---
+++ front/src/components/board/BoardWrite.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container mt-3">
+    <div class="row">
+      <div class="col">
+        <div class="alert alert-primary"><h3>글작성</h3></div>
+      </div>
+    </div>
+    <board-input-item type="register" />
+  </div>
+</template>
+
+<script>
+import BoardInputItem from "@/components/board/item/BoardInputItem";
+
+export default {
+  name: "BoardWrite",
+  components: {
+    BoardInputItem,
+  },
+};
+</script>
+
+<style></style>

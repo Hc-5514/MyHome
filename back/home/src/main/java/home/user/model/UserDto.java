88a5346a9430b795ecc92437a7fb14b05146e901
leejunejae <oune@kumoh.ac.kr>
2023-05-24 17:35:22 +0900

    feat: add user dto

--- back/home/src/main/java/home/user/model/UserDto.java
+++ back/home/src/main/java/home/user/model/UserDto.java
@@ -1,16 +1,21 @@
 package home.user.model;
 
-import lombok.*;
+import java.time.LocalDateTime;
 
-@Getter
-@Setter
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-public class UserDto {
-    String id;
-    String pwd;
-    String name;
-    String address;
-    String phone;
+public class UserDTO {
+	public String id;
+	public String password;
+	public String name;
+	public String email;
+	public LocalDateTime joinDate;
 }

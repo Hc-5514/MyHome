315edec88a79dcbbf1518e9ee17138829225988c
leejunejae <oune@kumoh.ac.kr>
2023-05-25 14:14:10 +0900

    feat: add userStore

--- front/src/store/modules/userStore.js
+++ front/src/store/modules/userStore.js
@@ -2,20 +2,28 @@ const userStore = {
     namespaced: true,
     state: {
         is_login: false,
+        user_id: "",
     },
     getters: {
         get_is_login: (state) => {
             return state.is_login
         },
+        get_user_id: (state) => {
+            return state.user_id
+        }
     },
     mutations: {
         set_is_login(state, is_login) {
             state.is_login = is_login
         },
+        set_user_id(state, id) {
+            state.user_id = id
+        }
     },
     actions: {
-        login(context) {
+        login(context, id) {
             context.commit('set_is_login', true)
+            context.commit('set_user_id', id)
         },
         logout(context) {
             context.commit('set_is_login', false)
